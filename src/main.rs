8e48852c668868966510e1f3ee134687f8b8e8e4
harleylizard <64570529+harleylizard@users.noreply.github.com>
2025-04-21 12:36:28 +1000

    bevy

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,5 @@
-mod window;
-mod wgpu;
-mod opengl;
-mod pipeline;
-mod mesh;
+use bevy::app::App;
 
 fn main() {
-    let mut window = window::Window::new();
-
-    let pipeline = pipeline::Pipeline::new(&window);
-    
-    while !window.should_close() {
-        window.poll();
-        
-        pipeline.draw();
-        
-    }
+  App::new().run();
 }
